8d590e1bb5001d5d09e7c9603130d00164df0125
Noah Stride <noah@noahstride.co.uk>
2021-03-17 17:27:01 +0000

    structure api more cleanly and start cmd/
    Signed-off-by: Noah Stride <noah@noahstride.co.uk>

--- cmd/pwgen/pwgen.go
+++ cmd/pwgen/pwgen.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/strideynet/pwgen"
+	"log"
+)
 
+func main() {
+	str, err := pwgen.Generate(pwgen.Number(12), pwgen.Length(18))
+	if err != nil {
+		log.Panicf("By golly, an error occured: %s", err)
+	}
+
+	log.Printf("The generated password is: %s", str)
 }
--- pwgen.go
+++ pwgen.go
@@ -1,37 +1,72 @@
 package pwgen
 
-import "log"
+import (
+	"errors"
+)
 
 type settings struct {
-	length int
-	uppercase int
+	length    int
 	lowercase int
-	number int
-	special int
+	number    int
+	special   int
+	uppercase int
 }
 
-func (s settings) processOptions(opts ...Option) settings  {
+type option func(*settings)
+
+// Length specifies the length of the password to return.
+func Length(l int) option {
+	return func(s *settings) {
+		s.length = l
+	}
+}
+
+// Lowercase specifies the minimum number of lowercase characters to include.
+func Lowercase(l int) option {
+	return func(s *settings) {
+		s.lowercase = l
+	}
+}
+
+// Uppercase specifies the minimum number of uppercase characters to include.
+func Uppercase(l int) option {
+	return func(s *settings) {
+		s.uppercase = l
+	}
+}
+
+// Number specifies the minimum number of numbers to include.
+func Number(l int) option {
+	return func(s *settings) {
+		s.number = l
+	}
+}
+
+// Special sets the minimum number of special characters to include (@%!?*^&).
+func Special(l int) option {
+	return func(s *settings) {
+		s.special = l
+	}
+}
+
+func Generate(opts ...option) (string, error) {
+	// start with default settings values
+	s := &settings{
+		length:    8,
+		uppercase: 2,
+		lowercase: 2,
+		number:    2,
+		special:   2,
+	}
+	// apply specified options to settings
 	for _, opt := range opts {
-		s = opt(s)
+		opt(s)
 	}
 
-	return s
-}
-
-var defaultSettings = settings{
-	length: 8,
-	uppercase: 2,
-	lowercase: 2,
-	number: 2,
-	special: 2,
-}
-
-type Option func(settings) settings
-
-func Generate(opts ...Option) (string, error) {
-	s := defaultSettings.processOptions(opts...)
-
-	log.Printf("%d", s.lowercase)
+	minimumLength := s.lowercase + s.number + s.special + s.uppercase
+	if minimumLength > s.length {
+		return "", errors.New("combined minimum lengths cannot exceed specified length")
+	}
 
 	return "", nil
 }
